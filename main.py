347c6773fd2857aa2063b7949d6438438ceae4bf
Prashant Singh <prashdash112@gmail.com>
2025-10-03 03:23:54 +0530

    executed modelling file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.preprocessing.preprocessing import *
 from src.preprocessing.handle_imbalance import *
+from src.modelling.model_train import * 
 
 def run():
     ####### data loading and preprocessing #######
@@ -19,33 +20,48 @@ def run():
 
     #########################################################
     # Configuration - Balancing data and train test split 
-    INPUT_preprocessed_FILE = "./src/data/creditcard_preprocessed.csv"
-    TRAIN_balanced_OUTPUT = "./src/data/creditcard_preprocessed_balanced_train.csv"
-    TEST_balanced_OUTPUT = "./src/data/creditcard_preprocessed_balanced_test.csv"
     
-    print("\n" + "="*60)
-    print("FRAUD DETECTION: CLASS IMBALANCE HANDLING")
-    print("="*60)
+    # INPUT_preprocessed_FILE = "./src/data/creditcard_preprocessed.csv"
+    # TRAIN_balanced_OUTPUT = "./src/data/creditcard_preprocessed_balanced_train.csv"
+    # TEST_balanced_OUTPUT = "./src/data/creditcard_preprocessed_balanced_test.csv"
     
-    # Load data
-    X, y = load_imbalanced_data(INPUT_preprocessed_FILE)
-    show_distribution(y, "Original Data")
+    # print("\n" + "="*60)
+    # print("FRAUD DETECTION: CLASS IMBALANCE HANDLING")
+    # print("="*60)
     
-    # Split and balance
-    X_train, X_test, y_train, y_test = split_and_balance(X, y, test_size=0.2)
+    # # Load data
+    # X, y = load_imbalanced_data(INPUT_preprocessed_FILE)
+    # show_distribution(y, "Original Data")
     
-    # Save processed data
-    feature_cols = [f'V{i}' for i in range(1, 29)] + ['Time', 'Amount']
-    save_balanced_data(X_train, y_train, TRAIN_balanced_OUTPUT, feature_cols)
-    save_balanced_data(X_test, y_test, TEST_balanced_OUTPUT, feature_cols)
+    # # Split and balance
+    # X_train, X_test, y_train, y_test = split_and_balance(X, y, test_size=0.2)
     
-    print("\n" + "="*60)
-    print("SUMMARY")
-    print("="*60)
-    print(f"Training samples: {len(y_train):,}")
-    print(f"Test samples: {len(y_test):,}")
-    print(f"\n✓ Ready for model training!")
-    print("="*60)
+    # # Save processed data
+    # feature_cols = [f'V{i}' for i in range(1, 29)] + ['Time', 'Amount']
+    # save_balanced_data(X_train, y_train, TRAIN_balanced_OUTPUT, feature_cols)
+    # save_balanced_data(X_test, y_test, TEST_balanced_OUTPUT, feature_cols)
+    
+    # print("\n" + "="*60)
+    # print("SUMMARY")
+    # print("="*60)
+    # print(f"Training samples: {len(y_train):,}")
+    # print(f"Test samples: {len(y_test):,}")
+    # print(f"\n✓ Ready for model training!")
+    # print("="*60)
+
+    ######################### MODELLING #######################
+    # File paths (can be overridden)
+    MODEL_TRAIN_FILE = "./src/data/creditcard_preprocessed_balanced_train.csv"
+    MODEL_TEST_FILE = "./src/data/creditcard_preprocessed_balanced_test.csv"
+    run_training_pipeline(
+    MODEL_TRAIN_FILE, 
+    MODEL_TEST_FILE,
+    train_rf=True,
+    train_xgb=True,
+    threshold=0.5, 
+    k=100,
+    serialize_best=True,
+    output_dir="models")
 
 
 if __name__ == "__main__":
